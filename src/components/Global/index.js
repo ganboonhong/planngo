de76107c2a93f43826717d6fcd6b1c12134fe57f
Gan Boon Hong <ganboonhong@gmail.com>
2017-04-30 00:35:05 +0800

    production

--- src/components/Global/index.js
+++ src/components/Global/index.js
@@ -1,8 +1,8 @@
 const 
 moment = require('moment-timezone'),
 obj    = {
-    // production: true,
-    production: false,
+    production: true,
+    // production: false,
     tz: 'Asia/Taipei',
     localDomain: 'http://127.0.0.1:9000',
     commonHint: 'Please check this field.',
