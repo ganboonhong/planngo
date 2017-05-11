9f4d68007feb9e98f290589fc546bfef364bf3ae
Gan Boon Hong <ganboonhong@gmail.com>
2017-05-11 21:38:39 +0800

    bug

--- server/global.js
+++ server/global.js
@@ -1,13 +1,15 @@
 const 
-ViewGlobal      = require('../src/components/Global'),
+ViewGlobal         = require('../src/components/Global'),
 // models          = require('./sequelize/models'),
 // sequelizeConfig = require( __dirname + '/sequelize/config/config.json')[ViewGlobal.env],
-moment          = ViewGlobal.moment,
-utcHr           = 8*60*60*1000;
+moment             = ViewGlobal.moment,
+production         = ViewGlobal.production,
+utcHr              = 8*60*60*1000;
 
 module.exports = {
     // models: models,
     // sequelizeConfig: sequelizeConfig,
     moment: moment,
-    utcHr: utcHr
+    utcHr: utcHr,
+    production: production,
 }
