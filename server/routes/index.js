26f6c0f9785545b372f3108f90b43dadf7c73e8b
Gan Boon Hong <ganboonhong@gmail.com>
2017-05-14 02:28:58 +0800

    revert

--- server/routes/index.js
+++ server/routes/index.js
@@ -34,8 +34,7 @@ module.exports = function(app){
         res.header('Access-Control-Allow-Credentials', true);
 
         // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
-        // res.header('Access-Control-Allow-Origin',  req.headers.origin);
-        res.header('Access-Control-Allow-Origin',  '*');
+        res.header('Access-Control-Allow-Origin',  req.headers.origin);
 
         // list of methods that are supported by the server
         res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,DELETE');
