22d5a4431410eddd161565d4528f95c745dd7483
Francis Gan <ganboonhong@users.noreply.github.com>
2017-05-20 15:32:56 +0800

    Revert "test 80 and 443"

--- server/index.js
+++ server/index.js
@@ -1,13 +1,7 @@
 'use strict';
 const app = require('./app');
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 80;
 
-const http = require('http');
-const https = require('https');
-
-http.createServer(app).listen(80);
-https.createServer(app).listen(443);
-
-// app.listen(PORT, ()=>{
-//     console.log(`App listening on port ${PORT}!`);
-// })
+app.listen(PORT, ()=>{
+    console.log(`App listening on port ${PORT}!`);
+})
