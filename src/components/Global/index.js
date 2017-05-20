ca7b631ccbc6898ccb511da20141a251c6568e4b
Gan Boon Hong <ganboonhong@gmail.com>
2017-05-20 15:35:32 +0800

    revert port to 9000

--- src/components/Global/index.js
+++ src/components/Global/index.js
@@ -4,7 +4,7 @@ obj    = {
     production: (process.env.NODE_ENV === 'development') ? false : true,
     // production: false,
     // tz: 'Asia/Taipei',
-    localDomain: 'http://127.0.0.1:80',
+    localDomain: 'http://127.0.0.1:9000',
     localReactDomain: 'http://127.0.0.1:3000',
     commonHint: 'Please check this field.',
     moment: moment,
