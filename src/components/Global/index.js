3f9e2a140cbe64e93beac2a3779e5f55fc40be1c
Gan Boon Hong <ganboonhong@gmail.com>
2017-05-20 15:05:24 +0800

    test port 80

--- src/components/Global/index.js
+++ src/components/Global/index.js
@@ -4,7 +4,7 @@ obj    = {
     production: (process.env.NODE_ENV === 'development') ? false : true,
     // production: false,
     // tz: 'Asia/Taipei',
-    localDomain: 'http://127.0.0.1:9000',
+    localDomain: 'http://127.0.0.1:80',
     localReactDomain: 'http://127.0.0.1:3000',
     commonHint: 'Please check this field.',
     moment: moment,
